ebacc20805ea680cf858608a816832522124181a
bloombar <2209098+bloombar@users.noreply.github.com>
2020-05-31 23:14:43 -0400

    adding two modes - local and github hosted

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# the directory that houses this file
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # get settings
@@ -14,5 +15,17 @@ echo "HOSTNAME: $HOSTNAME"
 DESTINATIONDIR==$(python3 $DIR/bash_brains.py destination_dir)
 echo "DESTINATION DIR: $DESTINATIONDIR"
 
-# upload files to server
-rsync -avz ./* $NETID@$HOSTNAME:$DESTINATIONDIR
+if [ $# -eq 0 ]
+  then
+    # running from local machine...
+    # upload files to server using local machine's default SSH keys
+    rsync -avz ./* $NETID@$HOSTNAME:$DESTINATIONDIR 
+  else
+    # running from GitHub build server...
+    # get the file path to SSH private key file from command line argument
+    $SSHIDFILEPATH=$1
+
+    # upload files to server using this SSH private key file
+    rsync -avz -e 'ssh -i $SSHIDFILEPATH $NETID@$HOSTNAME:$DESTINATIONDIR StrictHostKeyChecking=no' ./* $NETID@$HOSTNAME:$DESTINATIONDIR
+fi
+
